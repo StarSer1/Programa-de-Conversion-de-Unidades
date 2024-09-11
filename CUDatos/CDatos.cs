75db8344d309798fe38bf6ce8739d0b53c8dead4
wrok <brandonwrok@gmail.com>
2024-09-10 21:37:47 -0700

    miawmiawmiaw
    mooooi

--- CUDatos/CDatos.cs
+++ CUDatos/CDatos.cs
@@ -11,16 +11,31 @@ namespace CUDatos
 {
     public class CDatos
     {
+        public static double ConversorDeHertz(double Hertz, int opcion)
+        {
+            if (opcion == 1)
+                return Hertz;
+            else if (opcion == 2)
+                return Hertz * (Math.Pow(10, 3));
+            else if (opcion == 3)
+                return Hertz * (Math.Pow(10, 6));
+            else if (opcion == 4)
+                return Hertz * (Math.Pow(10, 9));
+            else if (opcion == 5)
+                return Hertz * (Math.Pow(10, 12));
+            else return 0;
+        }
+
         public static double ConversorDeFrecuencia(double segundos, int opcion)
         {
-
-            double milisegundos = segundos * (Math.Pow(10,3));    // 1 segundo = 1000 milisegundos
-            double microsegundos = segundos * (Math.Pow(10,6));    // 1 segundo = 1,000,000 microsegundos
-            double nanosegundos = segundos * (Math.Pow(10, 9));     // 1 segundo = 1,000,000,000 nanosegundos
-            double picosegundos = segundos * (Math.Pow(10, 12));    // 1 segundo = 1,000,000,000,000 picosegundos
+            double segundosI = 1 / segundos;
+            double milisegundos = segundosI * (Math.Pow(10, 3));    // 1 segundo = 1000 milisegundos
+            double microsegundos = segundosI * (Math.Pow(10, 6));    // 1 segundo = 1,000,000 microsegundos
+            double nanosegundos = segundosI * (Math.Pow(10, 9));     // 1 segundo = 1,000,000,000 nanosegundos
+            double picosegundos = segundosI * (Math.Pow(10, 12));    // 1 segundo = 1,000,000,000,000 picosegundos
 
             if (opcion == 1)
-                return segundos;
+                return segundosI;
             else if (opcion == 2)
                 return milisegundos;
             else if (opcion == 3)
@@ -32,7 +47,134 @@ namespace CUDatos
             else return 0;
 
         }
-        
-        
+
+
+        public static double ConversorDeSegundos(double segundos, int opcion)
+        {
+
+            double Herzios = 1/segundos;    // 1 Herzio = 1 segundos
+            double KiloHerzio = 1/(segundos * 1000);    // 1 KiloHerzio = 0.001 segundos
+            double MegaHerzio = 1 / (segundos * Math.Pow(10, 6));    // 1 Megaherzio = 0.000001 segundos
+            double GigaHerzio = 1 / (segundos * Math.Pow(10, 9));   // 1 Gigaherzio = 1.0E-9 segundos
+            double TeraHerzio = 1 / (segundos * Math.Pow(10, 12));  // 1 Teraherzio = 1.0E-12 segundos
+
+            if (opcion == 1)
+                return Herzios;
+            else if (opcion == 2)
+                return KiloHerzio;
+            else if (opcion == 3)
+                return MegaHerzio;
+            else if (opcion == 4)
+                return GigaHerzio;
+            else if (opcion == 5)
+                return TeraHerzio;
+            else return 0;
+        }
+
+        public static double ConversorDeMilisegundos(double milisegundos, int opcion)
+        {
+            // Convertir milisegundos a segundos
+            double segundos = milisegundos / 1000.0;
+
+            // Calcular las frecuencias en diferentes unidades
+            double Herzios = 1 / segundos;  // 1 Hertzio = 1 segundo
+            double KiloHerzio = 1 / (segundos * 1000);  // 1 KiloHerzio = 0.001 segundos
+            double MegaHerzio = 1 / (segundos * Math.Pow(10, 6));  // 1 Megaherzio = 0.000001 segundos
+            double GigaHerzio = 1 / (segundos * Math.Pow(10, 9));  // 1 Gigaherzio = 1.0E-9 segundos
+            double TeraHerzio = 1 / (segundos * Math.Pow(10, 12));  // 1 Teraherzio = 1.0E-12 segundos
+
+            // Devolver la frecuencia en la unidad solicitada
+            if (opcion == 1)
+                return Herzios;
+            else if (opcion == 2)
+                return KiloHerzio;
+            else if (opcion == 3)
+                return MegaHerzio;
+            else if (opcion == 4)
+                return GigaHerzio;
+            else if (opcion == 5)
+                return TeraHerzio;
+            else
+                return 0;
+        }
+        public static double ConversorDeMicrosegundos(double microsegundos, int opcion)
+        {
+            // Convertir microsegundos a segundos
+            double segundos = microsegundos / 1000000.0;
+
+            // Calcular las frecuencias en diferentes unidades
+            double Herzios = 1 / segundos;  // 1 Hertzio = 1 segundo
+            double KiloHerzio = 1 / (segundos * 1000);  // 1 KiloHerzio = 0.001 segundos
+            double MegaHerzio = 1 / (segundos * Math.Pow(10, 6));  // 1 Megaherzio = 0.000001 segundos
+            double GigaHerzio = 1 / (segundos * Math.Pow(10, 9));  // 1 Gigaherzio = 1.0E-9 segundos
+            double TeraHerzio = 1 / (segundos * Math.Pow(10, 12));  // 1 Teraherzio = 1.0E-12 segundos
+
+            // Devolver la frecuencia en la unidad solicitada
+            if (opcion == 1)
+                return Herzios;
+            else if (opcion == 2)
+                return KiloHerzio;
+            else if (opcion == 3)
+                return MegaHerzio;
+            else if (opcion == 4)
+                return GigaHerzio;
+            else if (opcion == 5)
+                return TeraHerzio;
+            else
+                return 0;
+        }
+        public static double ConversorDeNanosegundos(double nanosegundos, int opcion)
+        {
+            // Convertir nanosegundos a segundos
+            double segundos = nanosegundos / 1000000000.0;
+
+            // Calcular las frecuencias en diferentes unidades
+            double Herzios = 1 / segundos;  // 1 Hertzio = 1 segundo
+            double KiloHerzio = 1 / (segundos * 1000);  // 1 KiloHerzio = 0.001 segundos
+            double MegaHerzio = 1 / (segundos * Math.Pow(10, 6));  // 1 Megaherzio = 0.000001 segundos
+            double GigaHerzio = 1 / (segundos * Math.Pow(10, 9));  // 1 Gigaherzio = 1.0E-9 segundos
+            double TeraHerzio = 1 / (segundos * Math.Pow(10, 12));  // 1 Teraherzio = 1.0E-12 segundos
+
+            // Devolver la frecuencia en la unidad solicitada
+            if (opcion == 1)
+                return Herzios;
+            else if (opcion == 2)
+                return KiloHerzio;
+            else if (opcion == 3)
+                return MegaHerzio;
+            else if (opcion == 4)
+                return GigaHerzio;
+            else if (opcion == 5)
+                return TeraHerzio;
+            else
+                return 0;
+        }
+        public static double ConversorDePicosegundos(double picosegundos, int opcion)
+        {
+            // Convertir picosegundos a segundos
+            double segundos = picosegundos / 1000000000000.0;
+
+            // Calcular las frecuencias en diferentes unidades
+            double Herzios = 1 / segundos;  // 1 Hertzio = 1 segundo
+            double KiloHerzio = 1 / (segundos * 1000);  // 1 KiloHerzio = 0.001 segundos
+            double MegaHerzio = 1 / (segundos * Math.Pow(10, 6));  // 1 Megaherzio = 0.000001 segundos
+            double GigaHerzio = 1 / (segundos * Math.Pow(10, 9));  // 1 Gigaherzio = 1.0E-9 segundos
+            double TeraHerzio = 1 / (segundos * Math.Pow(10, 12));  // 1 Teraherzio = 1.0E-12 segundos
+
+            // Devolver la frecuencia en la unidad solicitada
+            if (opcion == 1)
+                return Herzios;
+            else if (opcion == 2)
+                return KiloHerzio;
+            else if (opcion == 3)
+                return MegaHerzio;
+            else if (opcion == 4)
+                return GigaHerzio;
+            else if (opcion == 5)
+                return TeraHerzio;
+            else
+                return 0;
+        }
+
     }
 }
--- EntornoDePruebas/Pruebas.cs
+++ EntornoDePruebas/Pruebas.cs
@@ -13,7 +13,7 @@ namespace EntornoDePruebas
         public void PruebaHertz()
         {
             double hertz = 50;
-            double resultado1 = 50000000000000;           
+            double resultado1 = 50000000000000;
             double resultadoGenereal;
             resultadoGenereal = CDatos.ConversorDeFrecuencia(hertz, 5);
             Assert.Equal(resultado1, resultadoGenereal);
@@ -21,16 +21,16 @@ namespace EntornoDePruebas
         [Fact]
         public void kiloHertz()
         {
-            double kiloHertz = 44;
-            double resultado2 = 44000;
+            double kiloHertz = CDatos.ConversorDeHertz(12, 2);
+            double resultado2 = 8.3333333333333E-5;
             double resultadoGenereal;
-            resultadoGenereal = CDatos.ConversorDeFrecuencia(kiloHertz, 2);
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(kiloHertz, 1);
             Assert.Equal(resultado2, resultadoGenereal);
         }
         [Fact]
         public void megaHertz()
         {
-            double megaHertz = 1 * (Math.Pow(10, 6)); ;
+            double megaHertz = CDatos.ConversorDeHertz(44, 3);
             double resultado3 = 1000000;
             double resultadoGenereal;
             resultadoGenereal = CDatos.ConversorDeFrecuencia(megaHertz, 3);
@@ -39,19 +39,249 @@ namespace EntornoDePruebas
         [Fact]
         public void gigaHertz()
         {
-            double gigaHertz = 1 * (Math.Pow(10, 9));
-            double resultado4 = 1000000000;
+            double gigaHertz = CDatos.ConversorDeHertz(5, 4);
+            double resultado4 = 2.0E-7;
             double resultadoGenereal;
-            resultadoGenereal = CDatos.ConversorDeFrecuencia(gigaHertz, 4);
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(gigaHertz, 2);
             Assert.Equal(resultado4, resultadoGenereal);
         }
         [Fact]
         public void teraHertz()
         {
-            double teraHertz = 44 * (Math.Pow(10, 12));
-            double resultado5 = 4.4E+25; 
+            double teraHertz = CDatos.ConversorDeHertz(44, 5);
+            double resultado5 = 0.022727272727273;
             double resultadoGenereal;
-            resultadoGenereal = CDatos.ConversorDeFrecuencia(teraHertz, 5);
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(teraHertz, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        //Segundos
+        [Fact]
+        public void Herzio()
+        {
+            double Segundos = 1;
+            double resultado5 = 1;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeSegundos(Segundos, 1);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void KiloHerzio()
+        {
+            double Segundos = 10;
+            double resultado5 = 0.0001;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeSegundos(Segundos, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void MegaHerzio()
+        {
+            double Segundos = 10;
+            double resultado5 = 1.0E-7;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeSegundos(Segundos, 3);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void GigaHerzio()
+        {
+            double GigaHerzio = 10;
+            double resultado5 = 1.0E-10;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeSegundos(GigaHerzio, 4);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void TeraHerzio()
+        {
+            double TeraHerzio = 1;
+            double resultado5 = 1.0E-12;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeSegundos(TeraHerzio, 5);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        //Milisegundos
+        [Fact]
+        public void Herzios()
+        {
+            double Milisegundos = 3;
+            double resultado5 = 333.33333333333;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeMilisegundos(Milisegundos, 1);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void KiloHerzios()
+        {
+            double Milisegundos = 1;
+            double resultado5 = 1;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeMilisegundos(Milisegundos, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void MegaHerzios()
+        {
+            double Milisegundos = 1;
+            double resultado5 = 0.001;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeMilisegundos(Milisegundos, 3);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void GigaHerzios()
+        {
+            double Milisegundos = 0.1;
+            double resultado5 = 1.0E-5;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeMilisegundos(Milisegundos, 4);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        [Fact]
+        public void TeraHerzios()
+        {
+            double Milisegundos = 0.1;
+            double resultado5 = 1.0E-8;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeMilisegundos(Milisegundos, 5);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+        // Microsegundos
+        [Fact]
+        public void HerziosDeMicrosegundos()
+        {
+            double microsegundos = 1;
+            double resultado5 = 1000000;
+            double resultadoGenereal = CDatos.ConversorDeMicrosegundos(microsegundos, 1);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void KiloHerziosDeMicrosegundos()
+        {
+            double microsegundos = 1;
+            double resultado5 = 1000;
+            double resultadoGenereal = CDatos.ConversorDeMicrosegundos(microsegundos, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void MegaHerziosDeMicrosegundos()
+        {
+            double microsegundos = 1;
+            double resultado5 = 1;
+            double resultadoGenereal = CDatos.ConversorDeMicrosegundos(microsegundos, 3);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void GigaHerziosDeMicrosegundos()
+        {
+            double microsegundos = 1;
+            double resultado5 = 1.0E-3;
+            double resultadoGenereal = CDatos.ConversorDeMicrosegundos(microsegundos, 4);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void TeraHerziosDeMicrosegundos()
+        {
+            double microsegundos = 1;
+            double resultado5 = 1.0E-6;
+            double resultadoGenereal = CDatos.ConversorDeMicrosegundos(microsegundos, 5);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        // Nanosegundos
+        [Fact]
+        public void HerziosDeNanosegundos()
+        {
+            double nanosegundos = 1;
+            double resultado5 = 1000000000;
+            double resultadoGenereal = CDatos.ConversorDeNanosegundos(nanosegundos, 1);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void KiloHerziosDeNanosegundos()
+        {
+            double nanosegundos = 1;
+            double resultado5 = 1000000;
+            double resultadoGenereal = CDatos.ConversorDeNanosegundos(nanosegundos, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void MegaHerziosDeNanosegundos()
+        {
+            double nanosegundos = 1;
+            double resultado5 = 1000;
+            double resultadoGenereal = CDatos.ConversorDeNanosegundos(nanosegundos, 3);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void GigaHerziosDeNanosegundos()
+        {
+            double nanosegundos = 1;
+            double resultado5 = 1;
+            double resultadoGenereal = CDatos.ConversorDeNanosegundos(nanosegundos, 4);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void TeraHerziosDeNanosegundos()
+        {
+            double nanosegundos = 1;
+            double resultado5 = 1.0E-3;
+            double resultadoGenereal = CDatos.ConversorDeNanosegundos(nanosegundos, 5);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        // Picosegundos
+        [Fact]
+        public void HerziosDePicosegundos()
+        {
+            double picosegundos = 1;
+            double resultado5 = 1000000000000;
+            double resultadoGenereal = CDatos.ConversorDePicosegundos(picosegundos, 1);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void KiloHerziosDePicosegundos()
+        {
+            double picosegundos = 1;
+            double resultado5 = 1000000000;
+            double resultadoGenereal = CDatos.ConversorDePicosegundos(picosegundos, 2);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void MegaHerziosDePicosegundos()
+        {
+            double picosegundos = 1;
+            double resultado5 = 1000000;
+            double resultadoGenereal = CDatos.ConversorDePicosegundos(picosegundos, 3);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void GigaHerziosDePicosegundos()
+        {
+            double picosegundos = 1;
+            double resultado5 = 1000;
+            double resultadoGenereal = CDatos.ConversorDePicosegundos(picosegundos, 4);
+            Assert.Equal(resultado5, resultadoGenereal);
+        }
+
+        [Fact]
+        public void TeraHerziosDePicosegundos()
+        {
+            double picosegundos = 1;
+            double resultado5 = 1;
+            double resultadoGenereal = CDatos.ConversorDePicosegundos(picosegundos, 5);
             Assert.Equal(resultado5, resultadoGenereal);
         }
     }
