7b53419224b36096b0ad926577b600a2f944a9e1
StarSer <androidemoc@gmail.com>
2024-09-10 20:09:07 -0700

    Primera conversion

--- CUDatos/CDatos.cs
+++ CUDatos/CDatos.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace CUDatos
 {
     public class CDatos
     {
-        public static bool numeroxd(int numeroxd, int otronumeroxd)
+        public static double ConversorDeFrecuencia(double segundos, int opcion)
         {
-            bool pendejotrue = false;
-            if (numeroxd == otronumeroxd)
-            {
-                pendejotrue = true;
-                return pendejotrue;
-            }
-            return pendejotrue;
+
+            double milisegundos = segundos * (Math.Pow(10,3));    // 1 segundo = 1000 milisegundos
+            double microsegundos = segundos * (Math.Pow(10,6));    // 1 segundo = 1,000,000 microsegundos
+            double nanosegundos = segundos * (Math.Pow(10, 9));     // 1 segundo = 1,000,000,000 nanosegundos
+            double picosegundos = segundos * (Math.Pow(10, 12));    // 1 segundo = 1,000,000,000,000 picosegundos
+
+            if (opcion == 1)
+                return segundos;
+            else if (opcion == 2)
+                return milisegundos;
+            else if (opcion == 3)
+                return microsegundos;
+            else if (opcion == 4)
+                return nanosegundos;
+            else if (opcion == 5)
+                return picosegundos;
+            else return 0;
+
         }
+        
+        
     }
 }
--- EntornoDePruebas/Pruebas.cs
+++ EntornoDePruebas/Pruebas.cs
@@ -10,13 +10,49 @@ namespace EntornoDePruebas
     public class Pruebas
     {
         [Fact]
-        public void Prueba1()
+        public void PruebaHertz()
         {
-            int n = 1;
-            int n2 = 1;
-            bool resultado;
-            resultado = CDatos.numeroxd(n, n2);
-            Assert.True(resultado);
+            double hertz = 50;
+            double resultado1 = 50000000000000;           
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(hertz, 5);
+            Assert.Equal(resultado1, resultadoGenereal);
+        }
+        [Fact]
+        public void kiloHertz()
+        {
+            double kiloHertz = 44;
+            double resultado2 = 44000;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(kiloHertz, 2);
+            Assert.Equal(resultado2, resultadoGenereal);
+        }
+        [Fact]
+        public void megaHertz()
+        {
+            double megaHertz = 1 * (Math.Pow(10, 6)); ;
+            double resultado3 = 1000000;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(megaHertz, 3);
+            Assert.Equal(resultado3, resultadoGenereal);
+        }
+        [Fact]
+        public void gigaHertz()
+        {
+            double gigaHertz = 1 * (Math.Pow(10, 9));
+            double resultado4 = 1000000000;
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(gigaHertz, 4);
+            Assert.Equal(resultado4, resultadoGenereal);
+        }
+        [Fact]
+        public void teraHertz()
+        {
+            double teraHertz = 44 * (Math.Pow(10, 12));
+            double resultado5 = 4.4E+25; 
+            double resultadoGenereal;
+            resultadoGenereal = CDatos.ConversorDeFrecuencia(teraHertz, 5);
+            Assert.Equal(resultado5, resultadoGenereal);
         }
     }
 }
